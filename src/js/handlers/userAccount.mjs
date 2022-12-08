84f0fe20f0b48cd87d0f890b6e19ecaa34049089
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-12-08 12:28:20 +0100

    renamed form handler function and started on JSdocs for the function

--- src/js/handlers/userAccount.mjs
+++ src/js/handlers/userAccount.mjs
@@ -1,16 +1,19 @@
 import { authUser } from "../api/auth/userAccount.mjs";
 
-export function setForm(userForm) {
+/**
+ *
+ * @param {HTMLElement} userForm <form> element.
+ *
+ */
+export function formListener(userForm) {
   userForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log("event is triggered");
 
     const form = event.target;
     const formData = new FormData(form);
     const profile = Object.fromEntries(formData.entries());
     const method = form.method;
     const action = form.attributes.action.value;
-    console.log("form actions is set");
     authUser(profile, action, method);
   });
 }
--- src/js/router.mjs
+++ src/js/router.mjs
@@ -1,13 +1,13 @@
-import { setForm } from "./handlers/userAccount.mjs";
+import { formListener } from "./handlers/userAccount.mjs";
 import * as form from "./formVariables/index.mjs";
 
 const path = location.pathname;
 
 switch (path) {
   case "/profile/register/":
-    setForm(form.register);
+    formListener(form.register);
     break;
   case "/profile/login/":
-    setForm(form.login);
+    formListener(form.login);
     break;
 }
