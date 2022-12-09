e0f8b4adf128f9a19c3a3ebfda59d522bf4808ae
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-12-09 16:10:29 +0100

    JSdcoumented apiRequest function

--- src/js/api/components/fetch.mjs
+++ src/js/api/components/fetch.mjs
@@ -1,3 +1,10 @@
+/**
+ * Sends a request to a server/api/url, converting the response to json, and returning it.
+ * catches any errors.
+ * @param {string} URL The url which to send the request.
+ * @param {object} options the options/information to include in the request(send to the server)
+ * @returns {object} response from the request in json format.
+ */
 export async function apiRequest(URL, options) {
   try {
     const response = await fetch(URL, options);
@@ -7,6 +14,6 @@ export async function apiRequest(URL, options) {
   } catch (error) {
     return error;
   } finally {
-    console.log("loading complete");
+    console.log("dev note: javascript fetch() method finished");
   }
 }
