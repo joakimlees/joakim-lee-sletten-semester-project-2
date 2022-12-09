dac826633e25613890115cecd850670e27aeba38
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-12-09 15:55:32 +0100

    JSdocumented authUser function

--- src/js/api/auth/userAccount.mjs
+++ src/js/api/auth/userAccount.mjs
@@ -1,6 +1,35 @@
 import { API_AUCTION_URL } from "../constants.mjs";
 import { apiRequest } from "../components/fetch.mjs";
 
+/**
+ * Sends a request to the AUCTION API/server, using the apiRequest function.
+ * Setting the fetch() options for the request.
+ *
+ * @param {object} profile containing key, value paris, which makes
+ * out the "body" in the fetch() options, i.e. user information such as: name, email, password etc.
+ * @param {string} action a API URL endpoint, for sending/receiving data to/from.
+ * @param {string} method method for the fetch() options, i.e. GET, POST, PUT, PATCH, DELETE.
+ *
+ * * @example
+ * ```js
+ * // Sends a request to the AUCTION API/server.
+ * Setting the fetch() options for the request.
+ *
+ * const profile = {
+ * name: yourName,
+ * email: example@stud.noroff.no,
+ * password: answer123,
+ * };
+ *
+ * const action = "/url/endpoint";
+ *
+ * const method = "post";
+ *
+ * authUser(profile, action, method);
+ *
+ * ```
+ *
+ */
 export async function authUser(profile, action, method) {
   const authURL = API_AUCTION_URL + action;
   const headers = { "Content-Type": "application/json" };
