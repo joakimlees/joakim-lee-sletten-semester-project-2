4748a9dc8a2243fb4588bc94f1bcf9c701e47675
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-12-14 15:59:02 +0100

    started on bid post request

---
+++ src/js/api/listings/bid.mjs
@@ -0,0 +1,15 @@
+// /api/v1/auction/listings/<id>/bids
+import { API_AUCTION_URL } from "../constants.mjs";
+import { authorizedApiRequest } from "../fetch/authFetch.mjs";
+
+export function bid(listingDetails, action, method) {
+  const createListingURL = API_AUCTION_URL + action;
+  const body = JSON.stringify(listingDetails);
+
+  const authOptions = {
+    method,
+    body,
+  };
+
+  authorizedApiRequest(createListingURL, authOptions);
+}
