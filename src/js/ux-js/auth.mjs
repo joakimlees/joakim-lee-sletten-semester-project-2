a363414bd67e1476e38770a85038ccac7e1577ab
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-12-18 20:12:28 +0100

    added loading to listings page and logout user

--- src/js/ux-js/auth.mjs
+++ src/js/ux-js/auth.mjs
@@ -1,15 +1,19 @@
-import { load } from "../storage/load.mjs";
+import * as storage from "../storage/index.mjs";
 import { loggedIn, loggedOut } from "./components/navChanges.mjs";
 
 export function authUx(pathRegister, pathProfile) {
   const profileRegister = document.getElementById("nav-profile-register");
   const loginLogout = document.getElementById("nav-login-logout");
-  const profile = load("profile");
-  const token = load("token");
+  const profile = storage.load("profile");
+  const token = storage.load("token");
 
   if (token === null || token === undefined) {
     loggedOut(pathRegister, profileRegister, loginLogout);
   } else if (token.length >= 30 || profile.email || profile.email.includes("noroff")) {
     loggedIn(pathProfile, profileRegister, loginLogout);
+    loginLogout.addEventListener("click", () => {
+      storage.remove("profile");
+      storage.remove("token");
+    });
   }
 }
