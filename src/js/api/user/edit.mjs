8c8f7652109931721dc3223f008fc898e5152c22
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-12-18 22:30:28 +0100

    made ux for edit avatar

--- src/js/api/user/edit.mjs
+++ src/js/api/user/edit.mjs
@@ -2,6 +2,7 @@ import { API_AUCTION_URL } from "../constants.mjs";
 import { authorizedApiRequest } from "../fetch/authFetch.mjs";
 import * as storage from "../../storage/index.mjs";
 import * as domain from "../../domains/index.mjs";
+import * as ux from "../../ux-js/userForms.mjs";
 
 export async function editProfile(avatar, username, method) {
   const editProfileURL = `${API_AUCTION_URL}/profiles/${username}/media`;
@@ -12,6 +13,21 @@ export async function editProfile(avatar, username, method) {
     body,
   };
 
+  const primaryBtn = document.querySelector("#edit-btn");
+  const secondaryBtn = document.querySelector("#discard-btn");
+  const editSuccess = document.querySelector("#edit-success");
+  const editError = document.querySelector("#edit-error");
+
   const updatedProfile = await authorizedApiRequest(editProfileURL, authOptions);
-  storage.save("profile", new domain.UserObject(updatedProfile.name, updatedProfile.email, updatedProfile.credits, updatedProfile.avatar));
+
+  if ("errors" in updatedProfile) {
+    ux.loginFailure(editError, "Something went wrong. Make sure to use a valid image url");
+  } else {
+    editSuccess.innerHTML = "Avatar updated";
+    editError.classList.add("d-none");
+    primaryBtn.classList.add("d-none");
+    secondaryBtn.className = "btn btn-success w-100";
+    secondaryBtn.innerHTML = "Back to profile";
+    storage.save("profile", new domain.UserObject(updatedProfile.name, updatedProfile.email, updatedProfile.credits, updatedProfile.avatar));
+  }
 }
